ea3fec3dc22995cbf357aabf2e699c2e52f3f007
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-03-23 03:37:12 +0430

    add CRUDTest XUnit Test and implement CountriesServiceTest class

---
+++ CRUDTest/CountriesServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using ServiceContracts;
+using Entities;
+using System;
+
+
+namespace CRUDTest
+{
+	public class CountriesServiceTest
+	{
+		private readonly ICountriesService _countriesService;
+
+		public CountriesServiceTest()
+		{
+			_countriesService = null;
+		}
+	}
+}
---
+++ CRUDTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CRUDTest
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
