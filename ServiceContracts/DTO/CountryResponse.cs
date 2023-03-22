66839eeb3c3c27d0d02b6c6382d09cdd17da97e1
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-03-23 03:20:44 +0430

    add ServiceContract library class   implement CountryAddRequest class (DTO) and CountryResponse clase (DTO)

---
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Entities;
+
+namespace ServiceContracts.DTO
+{
+	/// <summary>
+	/// DTO class is used return type for CountryService
+	/// </summary>
+	public class CountryResponse
+	{
+		public Guid CountryId { get; set; }
+		public string? CountyrName { get; set; }
+
+	}
+
+	public static class CountryExtensions
+	{
+		public static CountryResponse ToCountryResponse(this Country country)
+		{
+			return new CountryResponse
+			{
+				CountryId = country.CountryId,
+				CountyrName = country.CountryName
+			};
+		}
+	}
+}
