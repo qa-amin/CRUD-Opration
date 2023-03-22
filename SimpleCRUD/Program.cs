4c4b5f24e26f496a7c45238ae2a5c5c3416e176a
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-03-23 02:56:27 +0430

    add basic middleWare and add wwwroot folder

--- SimpleCRUD/Program.cs
+++ SimpleCRUD/Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Builder;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+if (builder.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+
+
+
 
 app.Run();
