7529c597bac96c7620e7191464762966ae8d38ec
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-03-23 03:03:45 +0430

    add and implement Entities Library class

---
+++ Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+	/// <summary>
+	/// Domin Model for Country
+	/// </summary>
+	public class Country
+	{
+		public Guid CountryId { get; set; }
+		public string? CountryName { get; set; }
+
+	}
+}
