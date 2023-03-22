26795821d099c0160a6e92126e5545aea2a5d509
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-03-23 03:29:31 +0430

    implement ICountriesService interface

--- ServiceContracts/ICountriesService.cs
+++ ServiceContracts/ICountriesService.cs
@@ -1,7 +1,17 @@
-﻿namespace ServiceContracts
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+namespace ServiceContracts
 {
+	/// <summary>
+	/// Represents business logic for manipulating Counrty Entity
+	/// </summary>
 	public interface ICountriesService
 	{
-
+		/// <summary>
+		/// add a country object to the list of countries
+		/// </summary>
+		/// <param name="countryAddRequest">Country object to add</param>
+		/// <returns>Retruns the country object after adding it</returns>
+		CountryResponse AddCountry(CountryAddRequest countryAddRequest);
 	}
 }
