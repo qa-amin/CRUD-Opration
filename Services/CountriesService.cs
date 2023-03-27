0cc4833398f30dd4f9c64b38b3438c11e333ece6
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-03-28 02:01:51 +0430

    implement CountriesService class  get object from CountryAddRequest convert to Country type genarate CountryId  add Country object to List<Country> _countries  convert Country object to CountryResponse and  some validation

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -1,13 +1,50 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
+using System.Collections.Generic;
 
 namespace Services
 {
 	public class CountriesService : ICountriesService
 	{
+		private readonly List<Country> _contries;
+
+		public CountriesService()
+		{
+			_contries = new List<Country> ();
+		}
+
 		public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
 		{
-			throw new NotImplementedException();
+			// validation CountryAddRequest  ArgumentNullException when it is null
+			if (countryAddRequest == null)
+			{
+				throw new ArgumentNullException(nameof(CountryAddRequest));
+			}
+
+			// validation CountryAddRequest.CountryName  ArgumentException when it is null
+			if (countryAddRequest.CountryName == null)
+			{
+				throw new ArgumentException(nameof(countryAddRequest.CountryName));
+			}
+
+			// validation CountryAddRequest.CountryName Duplication  ArgumentException when it is Duplicate
+			if (_contries.Where(c => c.CountryName == countryAddRequest.CountryName).Count() > 0)
+			{
+				throw new ArgumentException("Country name alredy exists!!!");
+			}
+			// convert object from CountryAddRequest to Country type
+			Country country = countryAddRequest.ToCountry();
+
+			// genarate CountryId
+			country.CountryId = Guid.NewGuid();
+
+			// add country object to _contries
+			_contries.Add(country);
+
+			CountryResponse countryResponse = country.ToCountryResponse();
+
+			return countryResponse;
 		}
 	}
 }
