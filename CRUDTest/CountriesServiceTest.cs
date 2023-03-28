b6a689af60953f492b38592d0dc7a72975f7b792
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-03-28 20:12:56 +0430

    add some method for test GetAllCountries() in CountriesServiceTest.cs

--- CRUDTest/CountriesServiceTest.cs
+++ CRUDTest/CountriesServiceTest.cs
@@ -14,7 +14,7 @@ namespace CRUDTest
 		{
 			_countriesService = new CountriesService();
 		}
-
+		#region addCountry
 		//When CountryAddRequest is null, it should throw ArgumentNullException
 		[Fact]
 		public void AddCountry_NullCountry()
@@ -75,6 +75,46 @@ namespace CRUDTest
 			//Assert
 			Assert.True(response.CountryId != Guid.Empty);
 		}
+		#endregion
+
+		#region GetAllCountries
+		[Fact]
+		//The list of countries should be empty by default (before adding any countries)
+		public void GetAllCountries_EmptyList()
+		{
+			//Act
+			List<CountryResponse> actual_country_response_list = _countriesService.GetAllCountries();
+
+			//Assert
+			Assert.Empty(actual_country_response_list);
+		}
+
+		[Fact]
+		public void GetAllCountries_AddFewCountries()
+		{
+			//Arrange
+			List<CountryAddRequest> country_request_list = new List<CountryAddRequest>() {
+			new CountryAddRequest() { CountryName = "IRAN" },
+			new CountryAddRequest() { CountryName = "USA" }
+	        };
+
+			//Act
+			List<CountryResponse> countries_list_from_add_country = new List<CountryResponse>();
+
+			foreach (CountryAddRequest country_request in country_request_list)
+			{
+				countries_list_from_add_country.Add(_countriesService.AddCountry(country_request));
+			}
+
+			List<CountryResponse> actualCountryResponseList = _countriesService.GetAllCountries();
+
+			//read each element from countries_list_from_add_country
+			foreach (CountryResponse expected_country in countries_list_from_add_country)
+			{
+				Assert.Contains(expected_country, actualCountryResponseList);
+			}
+		}
+		#endregion
 
 	}
 }
